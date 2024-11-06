75a21413f93b045e002c9c070a8ba23a3a5b1b10
zy111x <150530845+zy111x@users.noreply.github.com>
2024-11-06 22:13:03 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ export default function Footer() {
       <div >
         <p className="text-xs text-gray-500">Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于Cloudflare Pages，开源于
           <Link 
-          href="https://github.com/x-dr/telegraph-Image"
+          href="https://github.com/zy111x/telegraph-image-bed"
           className="text-blue-300  hover:text-red-900 ml-1"
           target="_blank"
           rel="noopener noreferrer"
-          >GitHub Telegraph-Image</Link> </p>
+          >GitHub telegraph-image-bed</Link> </p>
       </div>
     </footer>
   );
